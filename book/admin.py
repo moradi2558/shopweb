29a841ec7991ade26754555e1a0911e9115f1193
moradi2558 <mohammadhosainmoradi@gmai.com>
2025-01-21 11:39:03 +0330

    book admin added

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Book, Borrow
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price','date', 'isbn', 'available_copy', 'sell')
+    list_filter = ('date', 'category')
+    search_fields = ('name', 'isbn','category')
+
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'book', 'borrow_date', 'return_date', 'is_return')
+    list_filter = ('is_return', 'borrow_date')
+    search_fields = ('user__username', 'book__name')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Borrow, BorrowAdmin)
--- book/models.py
+++ book/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from account.models import User
+
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length = 250)
 
