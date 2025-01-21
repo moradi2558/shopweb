34ac798ae4d90880cd56309ca1fb756bd682f206
moradi2558 <mohammadhosainmoradi@gmai.com>
2025-01-21 16:22:37 +0330

    home view

--- book/views.py
+++ book/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework             import status
+from django.core.paginator import Paginator
+from .models import*
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+
+class Home(APIView):                              
+    def get(self,request,slug=None,id=None):
+
+        books = Book.objects.all()
+        page_size = self.request.query_params.get('limit',10)
+        page_number = self.request.query_params.get('page',1)
+
+        if slug and id:
+            data = get_object_or_404(Category,slug=slug,id=id)
+            Book.filter(category=data)
+
+
+        paginator = Paginator(books,page_size)
+        ser_data = BookSerializer(instance = paginator.page(page_number) ,many = True)
+        
+        context = {'data':ser_data.data,}
+
+        return Response(context,status = 200)
--- shopweb/urls.py
+++ shopweb/urls.py
@@ -5,4 +5,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('account.urls',namespace = 'account')),
+    path('',include('book.urls',namespace = 'allbook')),
 ]
